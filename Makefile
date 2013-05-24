6c8802e0a701a286f8f9f1c67600bd148c658dc1
task650 <task650@gmail.com>
2013-05-23 23:36:25 -0700

    Add all things to compile and my usual... Moar coming... Signed-off-by: task650 <task650@gmail.com>

--- Makefile
+++ Makefile
@@ -192,10 +192,8 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
-#ARCH		?= $(SUBARCH)
-#CROSS_COMPILE	?= $(CONFIG_CROSS_COMPILE:"%"=%)
-ARCH		?= arm
-CROSS_COMPILE	?= /opt/toolchains/arm-eabi-4.6/bin/arm-eabi-
+ARCH		?= $(SUBARCH)
+CROSS_COMPILE	?= $(CONFIG_CROSS_COMPILE:"%"=%)
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
@@ -354,10 +352,10 @@ CC		= $(srctree)/scripts/gcc-wrapper.py $(REAL_CC)
 
 CHECKFLAGS     := -D__linux__ -Dlinux -D__STDC__ -Dunix -D__unix__ \
 		  -Wbitwise -Wno-return-void $(CF)
-CFLAGS_MODULE   =
+CFLAGS_MODULE   = -fno-pic -mcpu=cortex-a15 -mtune=cortex-a15 -mfpu=neon-vfpv4
 AFLAGS_MODULE   =
 LDFLAGS_MODULE  =
-CFLAGS_KERNEL	=
+CFLAGS_KERNEL	= -mcpu=cortex-a15 -mtune=cortex-a15 -mfpu=neon-vfpv4
 AFLAGS_KERNEL	=
 CFLAGS_GCOV	= -fprofile-arcs -ftest-coverage
 
---
+++ build_jflteatt_aosp.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
+export INITRAMFS_SOURCE=`readlink -f ..`/Kernel_Stuff/Ramdisks/AOSP_JFLTE
+export CONFIG_AOSP_BUILD=y
+export PACKAGEDIR=$PARENT_DIR/Kernel_Stuff/Packages_jflteatt/AOSP_JFELTE_ATT
+#Enable FIPS mode
+export USE_SEC_FIPS_MODE=true
+export ARCH=arm
+# export CROSS_COMPILE=$PARENT_DIR/linaro4.5/bin/arm-eabi-
+export CROSS_COMPILE=/home/task650/AOKP/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+# export CROSS_COMPILE=$PARENT_DIR/linaro4.7/bin/arm-eabi-
+
+echo "Remove old Package Files"
+rm -rf $PACKAGEDIR/*
+
+echo "Setup Package Directory"
+mkdir -p $PACKAGEDIR/system/lib/modules
+
+echo "Create initramfs dir"
+mkdir -p $INITRAMFS_DEST
+
+echo "Remove old initramfs dir"
+rm -rf $INITRAMFS_DEST/*
+
+echo "Copy new initramfs dir"
+cp -R $INITRAMFS_SOURCE/* $INITRAMFS_DEST
+
+echo "chmod initramfs dir"
+chmod -R g-w $INITRAMFS_DEST/*
+rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print)
+rm -rf $(find $INITRAMFS_DEST -name .git -print)
+
+echo "Remove old zImage"
+rm $PACKAGEDIR/zImage
+rm arch/arm/boot/zImage
+
+echo "Make the kernel"
+make VARIANT_DEFCONFIG=underwear_jf_att_defconfig SELINUX_DEFCONFIG=jfselinux_defconfig jf_att_defconfig
+
+HOST_CHECK=`uname -n`
+if [ $HOST_CHECK = 'ktoonsez-VirtualBox' ] || [ $HOST_CHECK = 'task650-Underwear' ]; then
+	echo "Ktoonsez/task650 24!"
+	make -j24
+else
+	echo "Others! - " + $HOST_CHECK
+	make -j`grep 'processor' /proc/cpuinfo | wc -l`
+fi;
+
+echo "Copy modules to Package"
+cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
+cp ../Kernel_Stuff/Ramdisks/libsqlite.so $PACKAGEDIR/system/lib/libsqlite.so
+
+if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
+	echo "Copy zImage to Package"
+	cp arch/arm/boot/zImage $PACKAGEDIR/zImage
+
+	echo "Make boot.img"
+	./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img 
+	export curdate=`date "+%m-%d-%Y"`
+	echo "Executing loki"
+	./loki_patch-linux-x86_64 boot abootatt.img $PACKAGEDIR/boot.img $PACKAGEDIR/boot.lok
+	rm $PACKAGEDIR/boot.img
+	cd $PACKAGEDIR
+	cp -R ../META-INF .
+	rm ramdisk.gz
+	rm zImage
+	rm ../Underwear-jflteatt*.zip
+	zip -r ../Underwear-jflteatt-$curdate.zip .
+	cd $KERNELDIR
+else
+	echo "KERNEL DID NOT BUILD! no zImage exist"
+fi;
