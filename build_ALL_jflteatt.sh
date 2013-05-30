1cf2f61633afa1e07c93cab065a89b0ad1f06912
task650 <task650@gmail.com>
2013-05-29 22:36:54 -0700

    Add build all script to build loki and non-loki
    Signed-off-by: task650 <task650@gmail.com>

---
+++ build_ALL_jflteatt.sh
@@ -0,0 +1,15 @@
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_jflteatt_aosp.sh
+echo "******************************"
+echo "*Done! Move on to the next!!!*"
+echo "******************************"
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_jflteatt_aosp_loki.sh
+
+
